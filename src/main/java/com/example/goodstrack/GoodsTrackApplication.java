0f82ac261bd62caa3425f72f66817bc71b30117f
Michael <michaelkivo977@gmail.com>
2024-07-10 18:51:15 +0300

    Create empty repository

---
+++ src/main/java/com/example/goodstrack/GoodsTrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.goodstrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsTrackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/goodstrack/GoodsTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goodstrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
