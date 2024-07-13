949cc8a3eb524c91248a055b7a8c1ad7eeac1ae3
Michael <michaelkivo977@gmail.com>
2024-07-13 20:29:29 +0300

    Add the repositories and services packages

---
+++ src/main/java/com/example/goodstrack/services/ServiceManager.java
@@ -0,0 +1,19 @@
+package com.example.goodstrack.services;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+import java.util.HashSet;
+
+@Service
+public class ServiceManager {
+
+    @PersistenceContext
+    private EntityManager entitymanager;
+
+    @Transactional
+    public void initializeData() {
+        
+    }
+}
