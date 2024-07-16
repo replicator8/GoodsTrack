64262025b06a2dc3ba2e16080b0dae9528a99518
Michael <michaelkivo977@gmail.com>
2024-07-16 10:06:36 +0300

    Add the controllers package

--- src/main/java/com/example/goodstrack/GoodsTrackApplication.java
+++ src/main/java/com/example/goodstrack/GoodsTrackApplication.java
@@ -7,18 +7,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GoodsTrackApplication implements CommandLineRunner {
-
-    @Autowired
-    private ServiceManager manager;
+public class GoodsTrackApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GoodsTrackApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        manager.initializeData();
     }
 
 }
