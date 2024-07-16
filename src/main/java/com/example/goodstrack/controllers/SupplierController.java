ade05cc4687585f84fe805d50578444ee4b2e6b6
Michael <michaelkivo977@gmail.com>
2024-07-16 10:15:34 +0300

    Add nessecary libraries to controllers

--- src/main/java/com/example/goodstrack/controllers/SupplierController.java
+++ src/main/java/com/example/goodstrack/controllers/SupplierController.java
@@ -1,4 +1,10 @@
 package com.example.goodstrack.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SupplierController {
 }
